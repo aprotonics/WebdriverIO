6da98478c25b0e5ba7e5c3a5c92b302234f9c7a7
aprotonics <aprotonics@yandex.ru>
2022-03-03 13:52:07 +0500

    add page object for filter test

---
+++ test/pageobjects/filter.page.js
@@ -0,0 +1,94 @@
+let Page = require('./page')
+
+
+class FilterPage extends Page {
+    constructor() {
+        super()
+        this.productsFilteredSelector = '.grid__products .grid-product'
+        this.productsFilteredTitlesSelector = '.grid__products .grid-product .grid-product__title-inner'
+    }
+
+    get lowPrice() { return $('.ec-filter--price .ec-filter__price-from input') }
+    get highPrice() { return $('.ec-filter--price .ec-filter__price-to input') }
+    get filtersCount() { return $('.ec-filters__applied-count') }
+    get productsContainer() { return $('.grid__products') }
+    get firstProduct() { return $('.grid__products .grid-product:first-child') }
+    get firstProductImg() { return $('.grid__products .grid-product:first-child .grid-product__image') }
+    get filterByInStockCheckbox() { return $('#checkbox-in_stock') }
+    get filterByDiscountCheckbox() { return $('#checkbox-on_sale') }
+
+    async open(path) {
+        await super.open(path)
+    }
+
+    async filterByPrice(lowPriceValue, highPriceValue) {
+        await this.lowPrice.setValue(lowPriceValue)
+        await this.highPrice.setValue(highPriceValue)
+        await this.highPrice.addValue('\uE007')
+        await this.waitFilterToApply()
+    }
+
+    async filterByInStock() {
+        await this.filterByInStockCheckbox.click()
+        await this.waitFilterToApply()
+    }
+
+    async filterByDiscount() {
+        await this.filterByDiscountCheckbox.click()
+        await this.waitFilterToApply()
+    }
+
+    async checkFilterByPrice(productsInPriceAmount) {
+        let productsFilteredAmount = await this.countProducts()
+        let uniqueProductsFilteredAmount = await this.countUniqueProducts()
+
+        await expect(productsFilteredAmount).toEqual(productsInPriceAmount)
+        console.log(`${uniqueProductsFilteredAmount} == ${productsInPriceAmount}`)
+        expect(uniqueProductsFilteredAmount).toEqual(productsInPriceAmount)
+    }
+
+    async checkFilterByInStock(productsInStockAmount) {
+        let productsFilteredAmount = await this.countProducts()
+        let uniqueProductsFilteredAmount = await this.countUniqueProducts()
+
+        await expect(productsFilteredAmount).toEqual(productsInStockAmount)
+        console.log(`${uniqueProductsFilteredAmount} == ${productsInStockAmount}`)
+        expect(uniqueProductsFilteredAmount).toEqual(productsInStockAmount)
+    }
+
+    async checkFilterByDiscount(productsWithStrikeAmount) {
+        let productsFilteredAmount = await this.countProducts()
+        let uniqueProductsFilteredAmount = await this.countUniqueProducts()
+
+        await expect(productsFilteredAmount).toEqual(productsWithStrikeAmount)
+        console.log(`${uniqueProductsFilteredAmount} == ${productsWithStrikeAmount}`)
+        expect(uniqueProductsFilteredAmount).toEqual(productsWithStrikeAmount)
+    }
+
+    async waitFilterToApply() {
+        await expect(this.filtersCount).toBeExisting()
+        await expect(this.filtersCount).toHaveText('(1)')
+        await expect(this.productsContainer).toBeExisting()
+        await expect(this.firstProduct).toBeExisting()
+        await expect(this.firstProductImg).toBeExisting()
+        await expect(this.firstProductImg).toBeClickable()
+    }
+
+    async countProducts() {
+        let productsFiltered = await $$(this.productsFilteredSelector)
+        let productsFilteredAmount = productsFiltered.length
+        return productsFilteredAmount
+    }
+
+    async countUniqueProducts() {
+        let productsFilteredTitles = await $$(this.productsFilteredTitlesSelector)
+        let productsFilteredNamesSet = new Set()
+        productsFilteredTitles.forEach(textBlock => {
+            productsFilteredNamesSet.add(textBlock.getText())
+        })
+        let uniqueProductsFilteredAmount = productsFilteredNamesSet.size
+        return uniqueProductsFilteredAmount
+    }
+}
+
+module.exports =  new FilterPage()
--- test/specs/ecwid_filter.spec.js
+++ test/specs/ecwid_filter.spec.js
@@ -1,84 +1,33 @@
+let FilterPage = require('../pageobjects/filter.page')
+
+
 describe('Test ecwid filter', () => {
     let URL = 'https://buy-in-10-seconds.company.site/search'
 
-    let filtersCountSelector = '.ec-filters__applied-count'
-    let productsContainerSelector = '.grid__products'
-    let firstProductSelector = '.grid__products .grid-product:first-child'
-    let firstProductImgSelector = '.grid__products .grid-product:first-child .grid-product__image'
-    let productsFilteredSelector = '.grid__products .grid-product'
-    let productsFilteredTitlesSelector = '.grid__products .grid-product .grid-product__title-inner'
-    let lowPriceSelector = '.ec-filter--price .ec-filter__price-from input'
-    let highPriceSelector = '.ec-filter--price .ec-filter__price-to input'
-    let filterByInStockCheckboxSelector = '#checkbox-in_stock'
-    let filterByDiscountCheckboxSelector = '#checkbox-on_sale'
-    
-    async function waitFilterToApply() {
-        await expect($(filtersCountSelector)).toBeExisting()
-        await expect($(filtersCountSelector)).toHaveText('(1)')
-        await expect($(productsContainerSelector)).toBeExisting()
-        await expect($(firstProductSelector)).toBeExisting()
-        await expect($(firstProductImgSelector)).toBeExisting()
-        await expect($(firstProductImgSelector)).toBeClickable()
-    }
-
-    async function countProducts() {
-        let productsFiltered = await $$(productsFilteredSelector)
-        let productsFilteredAmount = productsFiltered.length
-        return productsFilteredAmount
-    }
-
-    async function countUniqueProducts() {
-        let productsFilteredTitles = await $$(productsFilteredTitlesSelector)
-        let productsFilteredNamesSet = new Set()
-        productsFilteredTitles.forEach(textBlock => {
-            productsFilteredNamesSet.add(textBlock.getText())
-        })
-        let uniqueProductsFilteredAmount = productsFilteredNamesSet.size
-        return uniqueProductsFilteredAmount
-    }
 
     it('test filter by price', async () => {
+        let productsInPriceAmount = 2
         let lowPriceValue = 3
         let highPriceValue = 5
-        let productsInPriceAmount = 2
         
-        await browser.url(URL)
-
-        await $(lowPriceSelector).setValue(lowPriceValue)
-        await $(highPriceSelector).setValue(highPriceValue)
-        await $(highPriceSelector).addValue('\uE007')
-
-        await waitFilterToApply()        
-        let productsFilteredAmount = await countProducts()
-        let uniqueProductsFilteredAmount = await countUniqueProducts()
-        
-        await expect(productsFilteredAmount).toEqual(productsInPriceAmount)
-        expect(uniqueProductsFilteredAmount).toEqual(productsInPriceAmount)
+        await FilterPage.open(URL)
+        await FilterPage.filterByPrice(lowPriceValue, highPriceValue)
+        await FilterPage.checkFilterByPrice(productsInPriceAmount)
     })
 
     it('test filter by in stock', async () => {
         let productsInStockAmount = 5
 
-        await browser.url(URL)
-        await $(filterByInStockCheckboxSelector).click()
-        await waitFilterToApply()
-        let productsFilteredAmount = await countProducts()
-        let uniqueProductsFilteredAmount = await countUniqueProducts()
-
-        await expect(productsFilteredAmount).toEqual(productsInStockAmount)
-        expect(uniqueProductsFilteredAmount).toEqual(productsInStockAmount)
+        await FilterPage.open(URL)
+        await FilterPage.filterByInStock()
+        await FilterPage.checkFilterByInStock(productsInStockAmount)
     })
 
     it('test filter by discount', async () => {
         let productsWithStrikeAmount = 1
-
-        await browser.url(URL)
-        await $(filterByDiscountCheckboxSelector).click()
-        await waitFilterToApply()
-        let productsFilteredAmount = await countProducts()
-        let uniqueProductsFilteredAmount = await countUniqueProducts()
-
-        await expect(productsFilteredAmount).toEqual(productsWithStrikeAmount)
-        expect(uniqueProductsFilteredAmount).toEqual(productsWithStrikeAmount)
+        
+        await FilterPage.open(URL)
+        await FilterPage.filterByDiscount()
+        await FilterPage.checkFilterByDiscount(productsWithStrikeAmount) 
     })
 })
